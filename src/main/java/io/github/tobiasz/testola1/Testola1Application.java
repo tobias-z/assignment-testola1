4ad03c08433b37db3bcc787face7a6d36463d6bd
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-09-16 17:44:15 +0200

    initial commit

---
+++ src/main/java/io/github/tobiasz/testola1/Testola1Application.java
@@ -0,0 +1,13 @@
+package io.github.tobiasz.testola1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testola1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testola1Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tobiasz/testola1/Testola1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tobiasz.testola1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testola1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
